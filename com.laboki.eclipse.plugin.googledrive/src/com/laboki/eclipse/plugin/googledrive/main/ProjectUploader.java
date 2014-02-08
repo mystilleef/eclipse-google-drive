a6a54c97aeea5e7376245d115cf0e28ebd8126ea
lateef <lateef@viki>
2014-02-07 21:23:30 -0500

    Implement module that uploads projects to Google Drive.

---
+++ com.laboki.eclipse.plugin.googledrive/src/com/laboki/eclipse/plugin/googledrive/main/ProjectUploader.java
@@ -0,0 +1,76 @@
+package com.laboki.eclipse.plugin.googledrive.main;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
+
+import com.google.api.services.drive.Drive;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.laboki.eclipse.plugin.googledrive.events.DriveServiceEvent;
+import com.laboki.eclipse.plugin.googledrive.events.UploadProjectsEvent;
+import com.laboki.eclipse.plugin.googledrive.instance.EventBusInstance;
+import com.laboki.eclipse.plugin.googledrive.instance.Instance;
+import com.laboki.eclipse.plugin.googledrive.resources.FolderScanner;
+import com.laboki.eclipse.plugin.googledrive.task.Task;
+
+public final class ProjectUploader extends EventBusInstance {
+
+	private Drive drive;
+	private String id;
+	private final List<Instance> instances = Lists.newArrayList();
+
+	public ProjectUploader(final EventBus eventBus) {
+		super(eventBus);
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void eventHandler(final DriveServiceEvent event) {
+		this.drive = event.getDriveService();
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void eventHandler(final RootParentIdEvent event) {
+		this.id = event.getRootParentId();
+	}
+
+	@Subscribe
+	@AllowConcurrentEvents
+	public void eventHandler(final UploadProjectsEvent event) {
+		new Task() {
+
+			@Override
+			protected void execute() {
+				this.upload(event.getProjectNames());
+			}
+
+			private void upload(final List<String> names) {
+				final List<IResource> resources = new FolderScanner().scanProjects(names);
+				ProjectUploader.this.startService(new RecursiveUploader(ProjectUploader.this, EventBus.INSTANCE, ProjectUploader.this.drive, ProjectUploader.this.id, resources));
+			}
+		}.begin();
+	}
+
+	private void startService(final Instance instance) {
+		this.instances.add(instance.begin());
+	}
+
+	@Override
+	public Instance end() {
+		this.stopServices();
+		return this;
+	}
+
+	private void stopServices() {
+		for (final Instance instance : ImmutableList.copyOf(this.instances))
+			this.stopService(instance);
+	}
+
+	protected void stopService(final Instance instance) {
+		this.instances.remove(instance.end());
+	}
+}
