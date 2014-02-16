5de2c36dd3987779a9e553663758159911d6d2d6
Lateef Alabi-Oki <mystilleef@gmail.com>
2014-02-16 07:18:16 -0500

    Add constants for readability.

--- com.laboki.eclipse.plugin.googledrive/src/com/laboki/eclipse/plugin/googledrive/main/MimeTypeContext.java
+++ com.laboki.eclipse.plugin.googledrive/src/com/laboki/eclipse/plugin/googledrive/main/MimeTypeContext.java
@@ -12,13 +12,16 @@ import com.google.common.net.MediaType;
 public enum MimeTypeContext {
 	INSTANCE;
 
+	private static final String GOOGLE_DRIVE_FOLDER_MIMETYPE = "application/vnd.google-apps.folder";
+	private static final String GOOGLE_DRIVE_UNKNOWN_MIMETYPE = "application/vnd.google-apps.unknown";
+
 	public static String getMimeType(final IResource file) {
 		try {
 			Preconditions.checkNotNull(file, "Resource should not be null");
-			if (file.getType() != IResource.FILE) return EditorContext.GOOGLE_DRIVE_FOLDER_MIMETYPE;
+			if (file.getType() != IResource.FILE) return MimeTypeContext.GOOGLE_DRIVE_FOLDER_MIMETYPE;
 			return MimeTypeContext.getMediaType((IFile) file).toString();
 		} catch (final Exception e) {
-			return "application/vnd.google-apps.unknown";
+			return MimeTypeContext.GOOGLE_DRIVE_UNKNOWN_MIMETYPE;
 		}
 	}
 
